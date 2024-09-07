833090e4b44ab6da0f640bf5f04ee9cc80ef6cff
Shahid Iqbal <shahid.iqbal4213@gmail.com>
2024-09-07 23:29:07 +0500

    Update build.gradle.kts

--- reelsplayer/build.gradle.kts
+++ reelsplayer/build.gradle.kts
@@ -21,10 +21,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
