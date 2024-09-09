b7f2ac77e75039ea94095154c7d37bb5e6c29903
shahidzbi4213 <shahid.iqbal4213@gmail.com>
2024-09-09 12:31:32 +0500

    Cache Bug resolved

--- reelsplayer/build.gradle.kts
+++ reelsplayer/build.gradle.kts
@@ -20,11 +20,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
         }
     }
     compileOptions {
